0d70a6ace5099f17b1fe9cac5e29dd8ab11c1e92
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-06-14 22:44:41 +0100

    Fix font awesome icons

--- android/app/src/main/java/com/rnmealsapp/MainActivity.java
+++ android/app/src/main/java/com/rnmealsapp/MainActivity.java
@@ -1,9 +1,6 @@
 package com.rnmealsapp;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,13 +12,4 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "rnMealsApp";
   }
-  @Override
-   protected ReactActivityDelegate createReactActivityDelegate() {
-     return new ReactActivityDelegate(this, getMainComponentName()) {
-       @Override
-       protected ReactRootView createRootView() {
-         return new RNGestureHandlerEnabledRootView(MainActivity.this);
-       }
-     };
-   }
 }
