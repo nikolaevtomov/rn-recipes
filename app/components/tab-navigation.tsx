fe6021e062e38c573f91f15bfa543770588894fc
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-06-15 14:37:14 +0100

    Tab icons

--- app/components/tab-navigation.tsx
+++ app/components/tab-navigation.tsx
@@ -1,18 +1,39 @@
+import React from 'react';
 import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {
+  createBottomTabNavigator,
+  BottomTabBarProps,
+} from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import StackNavigation from './stack-navigation';
 import Favorites from './favorites';
 // import Fonts from '../utils/fonts';
-// import {COLOURS} from '../utils/colours';
+import {COLOURS} from '../utils/colours';
 
 const TabNavigation = createBottomTabNavigator(
   {
-    categories: StackNavigation,
-    favorites: Favorites,
+    categories: {
+      screen: StackNavigation,
+      navigationOptions: {
+        tabBarIcon: (props: BottomTabBarProps) => (
+          <Icon {...props} name="cutlery" size={30} />
+        ),
+      },
+    },
+    favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        tabBarIcon: (props: BottomTabBarProps) => (
+          <Icon {...props} name="star" size={30} selectionColor={'red'} />
+        ),
+      },
+    },
   },
   {
-    defaultNavigationOptions: {},
+    tabBarOptions: {
+      activeTintColor: COLOURS.black,
+    },
   },
 );
 
